d314c56dd0cdfbbce5d74f009c7b5b4bc4197085
Adam Belica <adam_belica@epam.com>
2020-09-21 15:07:43 +0200

    search page handling of dropdowns with xpath and JS

--- portalPacjenta/__init__.py
+++ portalPacjenta/__init__.py
@@ -1,1 +1,21 @@
 __all__ = ["chromeObjects", "configObjects", "pageObjects"]
+
+if __name__ == "__main__":
+
+    from chromeObjects import existingChrome
+    from pageObjects.searchPage import searchPage
+    from selenium.webdriver.common.by import By
+
+    try:
+        existingChrome.checkForChromeInstance("chrome.exe", "--remote-debugging-port=9222")
+        driver = existingChrome.getExistingChrome(r"C:\Users\Adam_Belica\Chrome Drive\chromedriver.exe")
+    except existingChrome.InstanceException:
+        existingChrome.runChrome(r"'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' --remote-debugging-port=9222")
+        driver = existingChrome.getExistingChrome(r"C:\Users\Adam_Belica\Chrome Drive\chromedriver.exe")
+    except:
+        raise
+
+    searchPage = searchPage(driver, 3)
+    searchPage.searchAndSelect(By.ID, "city", "text-higlight", "Kraków")
+    searchPage.searchAndSelect(By.ID, "serviceVariant", "multi-select-item.leaf-item", "Stomatolog")
+    searchPage.searchAndSelectDropdown(By.TAG_NAME, "app-select-multi-facilities", "pt-1", "ul. Opolska 114")
--- portalPacjenta/pageObjects/searchPage.py
+++ portalPacjenta/pageObjects/searchPage.py
@@ -3,28 +3,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-class portalPacjenta_searchPage:
+class searchPage:
 
     def __init__(self, driver, delay):
         self.driver = driver
         self.delay = delay
 
-    def searchAndSelect(self, by, field, dropdownClass, searchString, clear = False):
+    def searchAndSelect(self, by, field, dropdownClass, searchString):
         WebDriverWait(self.driver, self.delay).until(EC.invisibility_of_element((By.CLASS_NAME, "spinner-wrapper")))
-        mainElement = WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((by, field)))
-        mainElement.find_element_by_class_name("form-control.text-input.text-input-transparent.ng-untouched.ng-pristine.ng-valid").click()
-        #mainElement.find_element_by_xpath("//input[1]").click()
-        #form-control text-input ng-untouched ng-pristine ng-valid text-input-transparent
-        mainElement.find_element_by_class_name("form-control.text-input.text-input-transparent.ng-untouched.ng-pristine.ng-valid").send_keys(searchString)
+        mainElement = self.driver.find_elements_by_xpath("//*[@id='{}']//input".format(field))[0] #WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((by, field)))
+        self.driver.execute_script("arguments[0].click();", mainElement)
+        mainElement = self.driver.find_elements_by_xpath("//*[@id='{}']//*[text() = '{}']".format(field, searchString))[0]
+        self.driver.execute_script("arguments[0].click();", mainElement)
 
-        for el in mainElement.find_elements_by_class_name(dropdownClass):
-            if searchString == el.text:
-                el.click()
-                break
-        
-        if clear:
-            mainElement.find_element_by_class_name("form-control.text-input.ng-valid.ng-dirty.ng-touched.text-input-full.dropdown-opened").clear()
-            self.driver.execute_script("return document.getElementsByClassName('form-control text-input ng-valid ng-dirty ng-touched text-input-transparent dropdown-opened')[0].blur();")
 
     def searchAndSelectDropdown(self, by, field, dropdownClass, *searchStrings):
         WebDriverWait(self.driver, self.delay).until(EC.invisibility_of_element((By.CLASS_NAME, "spinner-wrapper")))
